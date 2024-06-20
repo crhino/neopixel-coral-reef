f4c5f19714eaa66d4d61228d879b66d90a6d9694
Chris Piraino <cpiraino@hashicorp.com>
2024-06-19 21:12:08 -0400

    Fix compilation

--- main.py
+++ main.py
@@ -1,17 +1,34 @@
 import itertools
 import board
 import neopixel
-from adafruit_led_animation.animation.color import CYAN, BLUE, AQUA, GREEN
+from adafruit_led_animation.color import CYAN, BLUE, AQUA, GREEN
 from adafruit_led_animation.animation.colorcycle import ColorCycle
-from adafruit_led_animation.animation.solid import Solid
 from adafruit_led_animation.animation.helper import PixelSubset
 from adafruit_led_animation.sequence import AnimationSequence
 from adafruit_led_animation.group import AnimationGroup
 
+# construct color cycles given start and end colors
+
+
+def color_cycle_array(start, end, steps):
+    list(map(lambda step: interpolate_color_between(
+        start, end, step/steps), itertools.count(steps)))
+
+
+def interpolate_color_between(color1, color2, ratio):
+    r1, g1, b1 = color1
+    r2, g2, b2 = color2
+    r = int(r1 + (r2 - r1) * ratio)
+    g = int(g1 + (g2 - g1) * ratio)
+    b = int(b1 + (b2 - b1) * ratio)
+    return (r, g, b)
+
+
 pixel_pin = board.D18
 pixel_num = 50
 
-pixels = neopixel.NeoPixel(pixel_pin, pixel_num, brightness=1, auto_write=False)
+pixels = neopixel.NeoPixel(
+    pixel_pin, pixel_num, brightness=1, auto_write=False)
 
 # Objects on Pillar
 rock = PixelSubset(pixels, 49, 49)
@@ -45,23 +62,10 @@ animations = AnimationSequence(
     ),
 )
 
-#main loop
+# main loop
 try:
     while True:
         animations.animate()
 except KeyboardInterrupt:
     pixels.fill((0, 0, 0))
     pixels.show()
-
-# construct color cycles given start and end colors
-def color_cycle_array(start, end, steps):
-    list(map(lambda step : interpolate_color_between(start, end, step/steps), itertools.count(steps)))
-
-def interpolate_color_between(color1, color2, ratio):
-    r1, g1, b1 = color1
-    r2, g2, b2 = color2
-    r = int(r1 + (r2 - r1) * ratio)
-    g = int(g1 + (g2 - g1) * ratio)
-    b = int(b1 + (b2 - b1) * ratio)
-    return (r, g, b)
-
